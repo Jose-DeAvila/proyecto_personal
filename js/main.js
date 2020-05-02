2b2b791ef22c21c7e978fb5d66f9b5d8b2113537
Jose De Avila <joseluisdeavila01@gmail.com>
2020-05-02 13:13:53 -0500

    empezando

---
+++ js/main.js
@@ -0,0 +1,19 @@
+const typed = new Typed('.typed', {
+    strings: [
+        '<i class="palabra">Perfecta</i>', 
+        '<i class="palabra">Amable</i>', 
+        '<i class="palabra">Amorosa</i>',
+        '<i class="palabra">Interesante</i>',
+        '<i class="palabra">Alegre</i>',
+        '<i class="palabra">Mi amor</i>'
+    ],
+    typeSpeed: 75,
+    backSpeed: 75,
+    startDelay: 300,
+    smartBackspace: false,
+    shuffle: true,
+    backDelay: 1000,
+    loop: true,
+    showCursor: true,
+    cursorChar: '♥'
+});
